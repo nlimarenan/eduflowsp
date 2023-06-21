dbe8bf5ed7670f705c5006760df68a06561f3a55
Renan Lima <nlimarenan@gmail.com>
2023-06-21 00:18:42 -0300

    first commit

---
+++ src/main/kotlin/com/edu/eduflowsp/EduflowspApplication.kt
@@ -0,0 +1,11 @@
+package com.edu.eduflowsp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EduflowspApplication
+
+fun main(args: Array<String>) {
+	runApplication<EduflowspApplication>(*args)
+}
---
+++ src/test/kotlin/com/edu/eduflowsp/EduflowspApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.edu.eduflowsp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EduflowspApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
