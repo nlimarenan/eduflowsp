029cb1df7dffd25c28d918dedd4a58eee1559385
Renan Lima <nlimarenan@gmail.com>
2023-06-26 00:02:53 -0300

    inclusao de dados ao inicializar app/adicionar flyway para controle de migration

--- src/main/kotlin/com/edu/eduflowsp/EduflowspApplication.kt
+++ src/main/kotlin/com/edu/eduflowsp/EduflowspApplication.kt
@@ -1,5 +1,6 @@
 package com.edu.eduflowsp
 
+import org.flywaydb.core.Flyway
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,12 @@ import org.springframework.boot.runApplication
 class EduflowspApplication
 
 fun main(args: Array<String>) {
+
 	runApplication<EduflowspApplication>(*args)
+
+	val flyway = Flyway.configure()
+		.dataSource("jdbc:mysql://localhost:3306/eduflow","root","root")
+		.locations("classpath:db/migration")
+		.load()
+	flyway.migrate()
 }
